f3abdb7d0512038b86913207f7310729c618e6df
irfandi0101 <40199186+irfandi0101@users.noreply.github.com>
2018-06-12 17:15:46 +0800

    Add files via upload

---
+++ Admin.py
@@ -0,0 +1,65 @@
+import os
+
+import time
+
+
+from getpass import getpass
+
+
+def menu():
+
+    os.system('clear')
+
+    print('\033[1;36;40m v.1.1 [█ █ █ █ █ █ █ █ █ █ █ █ █ █]') 
+
+    print("")
+
+    
+
+    print('\033[1;92m 786 => bismi-llāhir-raḥmānir-raḥīm')
+
+    print("\033[1;93m")
+
+    print(" << --------Assalamu-Alaikum---------->>")
+
+    print("\033[1;96m")
+
+    x = input('\033[1;92mUsername \033[1;93m: ')
+
+    print("")
+
+    o = input("Are you want to show your password [ y/n ] : ")
+
+    print("")
+
+    if o=="y":
+
+        e = input('\033[1;92mPassword \033[1;93m: ')
+
+    elif o=="n":
+
+        e = getpass('\033[1;92mPassword \033[1;93m: ')
+
+
+    if x=="muju" and e=="123":
+
+        print('')
+
+        print(' welcome ========>',x)
+
+        print("")
+
+    else:
+
+         print("")
+
+         print("\033[1;91m You can't Access my Termux privacy 😜")
+
+         time.sleep(2)
+
+         os.system('ps')
+
+         os.system('killall -9 com.termux')
+
+menu()
+
